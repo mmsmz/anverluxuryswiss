9b0e52abe11ed77298d6d343235fe6690f1bd9b3
mohamed <marafath07@gmail.com>
2020-12-18 00:19:56 +0530

    asdasd

--- assets/js/main.js
+++ assets/js/main.js
@@ -625,37 +625,37 @@
 		prevArrow: '<button class="slick-prev"><i class="ion-ios-arrow-back"></i></button>',
 		nextArrow: '<button class="slick-next"><i class="ion-ios-arrow-forward"></i></button>',
 		responsive: [{
-				breakpoint: 1501,
-				settings: {
-					slidesToShow: 4
-				}
-			},
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 3
-				}
-			},
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 3
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 575,
-				settings: {
-					slidesToShow: 1
-				}
+			breakpoint: 1501,
+			settings: {
+				slidesToShow: 4
 			}
-		]
-	});
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		},
+		{
+			breakpoint: 575,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
+});
 	/*  Hiraola's Product Tab Slider Two
 /*----------------------------------------*/
 	$('.hiraola-product-tab_slider-2').slick({
